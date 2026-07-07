545bc19af1fc1c46c532b03bcb532360a16ac5ae
Headhunter5000 <headhunter5000@gmx.net>
2026-07-07 19:55:27 +0200

    add a delayed fade in of the PageLoader

--- src/components/common/PageLoader.tsx
+++ src/components/common/PageLoader.tsx
@@ -1,5 +1,19 @@
 import { Text } from 'grommet';
+import styled from 'styled-components';
 
-const PageLoader = () => <Text margin={{ top: 'large' }}>Loading...</Text>;
+const DelayedText = styled(Text)`
+  opacity: 0;
+  /* Name | Dauer | Timing-Function | Delay | Fill-Mode */
+  animation: fadeIn 0.3s ease-in-out 2s forwards;
+
+  @keyframes fadeIn {
+    to {
+      opacity: 1;
+    }
+  }
+`;
+
+const PageLoader = () =>
+  <DelayedText margin={{ top: 'large' }}>Loading...</DelayedText>;
 
 export default PageLoader;
