10bcf53b7034d76b53317f506b97cc39bbff2099
Headhunter5000 <headhunter5000@gmx.net>
2026-07-09 20:36:26 +0200

    make background grain calculation async

--- src/components/layout/GlobalStyle.tsx
+++ src/components/layout/GlobalStyle.tsx
@@ -1,4 +1,4 @@
-
+import { useEffect, useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import themeColors from '../../config/themeColors';
 import {
@@ -8,33 +8,74 @@ import {
 } from '../../utils/backgroundGrain';
 import { chooseByTheme } from '../../utils/themeUtils';
 
-const DEFAULT_BACKGROUND_GRAIN =
-  generateGrainImg({ size: 240, scale: 1, contrast: 4 });
+type GlobalStyleProps = {
+  bgLight: string;
+  bgDark: string;
+};
 
-const BG_LIGHT = encodeToBase64(mixGrainImgWithColor({
-  grain: DEFAULT_BACKGROUND_GRAIN,
-  bgColor: themeColors['sand-100'],
-  grainColor: 'black',
-  intensity: 0.17,
-}));
-
-const BG_DARK = encodeToBase64(mixGrainImgWithColor({
-  grain: DEFAULT_BACKGROUND_GRAIN,
-  bgColor: themeColors['slate-900'],
-  grainColor: 'white',
-  intensity: 0.07,
-}));
-
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle<GlobalStyleProps>`
   body {
     margin: 0;
   }
 
   #app-root {
-    background-image: url("${chooseByTheme(BG_LIGHT, BG_DARK)}");
+    background-image: url("${props => chooseByTheme(props.bgLight, props.bgDark)}");
     background-attachment: local;
     background-repeat: repeat;
   }
 `;
 
-export default GlobalStyle;
+const GlobalStyleWrapper = () => {
+  const [grainImages, setGrainImages] = useState<GlobalStyleProps>({
+    bgLight: '',
+    bgDark: '',
+  });
+
+  useEffect(() => {
+    // requestIdleCallback moves the workload to a time where the browser is idling
+    // Fallback to setTimeout  for Browsers not supporting requestIdleCallback.
+    const schedule =
+      window.requestIdleCallback ?? ((cb: () => void) => setTimeout(cb, 1));
+
+    const handle = schedule(() => {
+      const grain = generateGrainImg({ size: 240, scale: 1, contrast: 5 });
+
+      const bgLight = encodeToBase64(
+        mixGrainImgWithColor({
+          grain,
+          bgColor: themeColors['sand-100'],
+          grainColor: 'black',
+          intensity: 0.19,
+        }),
+      );
+
+      const bgDark = encodeToBase64(
+        mixGrainImgWithColor({
+          grain,
+          bgColor: themeColors['slate-900'],
+          grainColor: 'white',
+          intensity: 0.08,
+        }),
+      );
+
+      setGrainImages({ bgLight, bgDark });
+    });
+
+    return () => {
+      if (window.cancelIdleCallback) {
+        window.cancelIdleCallback(handle as number);
+      } else {
+        clearTimeout(handle as number);
+      }
+    };
+  }, []);
+
+  return (
+    <GlobalStyle
+      bgLight={grainImages.bgLight}
+      bgDark={grainImages.bgDark}
+    />
+  );
+};
+
+export default GlobalStyleWrapper;
