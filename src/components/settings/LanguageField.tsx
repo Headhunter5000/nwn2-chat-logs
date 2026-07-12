43d3fbc2b170f4ffd9a4d5609840ae9a92c1ae9f
Headhunter5000 <headhunter5000@gmx.net>
2026-07-12 19:32:51 +0200

    add french to the language selector

--- src/components/settings/LanguageField.tsx
+++ src/components/settings/LanguageField.tsx
@@ -2,22 +2,30 @@ import { FormField, RadioButtonGroup } from 'grommet';
 import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 
-const getOptions = (t: (i18nKey: string) => string): { value: string, label: string}[] => ([
-  { value: 'en', label: t('language.en') },
-  { value: 'de', label: t('language.de') },
-]);
+const browserLang = navigator.language;
+
+const OPTIONS: { value: string, label: string }[] = [
+  { value: 'en', label: 'English' },
+  { value: 'de', label: 'Deutsch' },
+  { value: 'fr', label: 'Français' },
+];
 
 export const LanguageField = () => {
   const { t } = useTranslation();
-  const options = useMemo(() => getOptions(t), [t]);
   const { i18n } = useTranslation();
+
+  const sortedOptions = useMemo(() => [...OPTIONS].sort((a, b) => {
+    if (browserLang.startsWith(a.value)) return -1;
+    if (browserLang.startsWith(b.value)) return 1;
+    return 0;
+  }), []);
 
   return (
     <FormField label={t('common.language')}>
       <RadioButtonGroup
         name="colorizeNames"
         direction="row"
-        options={options}
+        options={sortedOptions}
         value={i18n.resolvedLanguage}
         onChange={e => i18n.changeLanguage(e.target.value)}
       />
--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -4,14 +4,16 @@ import { initReactI18next } from 'react-i18next';
 
 import translationDE from '../locales/de/translation.yml';
 import translationEN from '../locales/en/translation.yml';
+import translationFR from '../locales/fr/translation.yml';
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      de: { translation: translationDE },
       en: { translation: translationEN },
+      de: { translation: translationDE },
+      fr: { translation: translationFR },
     },
     fallbackLng: 'en',
     interpolation: {
